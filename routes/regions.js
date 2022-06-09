0d7b0b82b03e5d2b5bc2905a0eeb581560fb9c60
Amit Karwande <karwande@gmail.com>
2022-06-08 21:44:59 -0700

    add nodemon and region related routes

---
+++ routes/regions.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+// middleware that is specific to this router
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+// define the home page route
+router.get('/', (req, res) => {
+  res.send('all regions')
+})
+
+// define the about route
+router.get('/:regionId', (req, res) => {
+  res.send('per region')
+})
+
+module.exports = router
