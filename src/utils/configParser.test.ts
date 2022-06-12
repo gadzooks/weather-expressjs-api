9e7b798d074da4ab7c890a5aa53e6981426d4b9f
akarwande <akarwande@coredigital.com>
2022-06-11 18:39:45 -0700

    exclude test files from being copied to /dist dir.

--- src/utils/configParser.test.ts
+++ src/utils/configParser.test.ts
@@ -5,14 +5,21 @@ import {loadRegions, RegionHash} from './configParser';
 describe(('loadRegions'), () => {
     let regionHash: RegionHash;
 
-    beforeEach(() => {
+    beforeAll(() => {
         regionHash = loadRegions();
     })
 
     it('should parse regions correctly', () => {
         expect(regionHash).toBeDefined();
+        expect(Object.keys(regionHash).length).toEqual(9);
         
-
+        const cities = regionHash['cities'];
+        expect(cities.description).toBeDefined();
+        expect(cities.name).toBeDefined();
+        expect(cities.search_key).toBeUndefined();
+        
+        const locations = cities.locations;
+        expect(locations.length).toBe(2);
     });
 
 });
