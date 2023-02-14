f63fd78ebbc6f69d54e00d3ded6becc77c44072f
akarwande <akarwande@coredigital.com>
2023-02-14 00:52:57 -0800

    add jwt auth for /forecasts paths

--- src/auth.ts
+++ src/auth.ts
@@ -9,22 +9,24 @@ export interface CustomRequest extends Request {
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   try {
-    console.log(JSON.stringify(req.header('Authorization')))
     const token = req.header('Authorization')?.replace('Bearer ', '')
 
-    console.log(token)
     if (!token) {
       throw new Error();
     }
 
-    console.log('key is ' + SECRET_KEY)
     const decoded = jwt.verify(token, SECRET_KEY);
     (req as CustomRequest).token = decoded;
 
-    console.log(JSON.stringify(decoded))
-    next();
+    // {"client":"weather-react-app","routes":["/forecasts/real","/forecasts/mock"],"type":"READONLY","iat":1676358327}
+    const routesAllowed = (<any>decoded).routes || []
+    if (routesAllowed.includes(req.baseUrl + req.path)) {
+      next();
+    } else {
+      res.status(401).send('Invalid / unauthorized route : ' + req.originalUrl);
+    }
   } catch (err) {
     console.log(JSON.stringify(err))
-    res.status(401).send('Please authenticate');
+    res.status(401).send('Please authenticate' + err);
   }
 };
